db275212bf17a8473ab8f0afcbf1409582d09428
Gabriel Marques <gabrielf.pmarques@gmail.com>
2016-10-29 21:11:39 -0200

    add reed switches of axis and door

--- code/code.ino
+++ code/code.ino
@@ -4,31 +4,47 @@ const int leftLdr = A1;
 //LASERS
 const int rightLaser = 2;
 const int leftLaser = 3;
+//BOTOES
+const int doorButton = 7;
+const int axisButton = 6;
 //RELAYS
 const int clockwiseEngine = 13;
 const int antiClockwiseEngine = 12;
 
 int rightLdrValue = 0;
 int leftLdrValue = 0;
+int doorState = 0;
+int axisState = 0;
+int lastAxisState = 0;
 
 void setup() {
   pinMode(clockwiseEngine, OUTPUT);
   pinMode(antiClockwiseEngine, OUTPUT);
   pinMode(rightLaser, OUTPUT);
   pinMode(leftLaser, OUTPUT);
+  pinMode(doorButton, INPUT);
+  pinMode(axisButton, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  turnOnLasers();
-  rightLdrValue = analogRead(rightLdr);
-  leftLdrValue = analogRead(leftLdr);
-//  Serial.println(rightLdrValue);
-//  Serial.println(leftLdrValue);
-//  delay(300);
-  if(hasObject(rightLdrValue, leftLdrValue)){
-    startMotor();
+  doorState = digitalRead(doorButton);
+  axisState = digitalRead(axisButton);
+  if(doorState == HIGH){
+    turnLasersOn();
+    rightLdrValue = analogRead(rightLdr);
+    leftLdrValue = analogRead(leftLdr);
+  //  Serial.println(rightLdrValue);
+  //  Serial.println(leftLdrValue);
+  //  delay(300);
+    if(hasObject(rightLdrValue, leftLdrValue)){
+      startMotor();
+      // TODO terminar aqui.. https://www.arduino.cc/en/Tutorial/StateChangeDetection
+    }else{
+      stopMotor();
+    }
   }else{
+    turnLasersOff();
     stopMotor();
   }
 }
@@ -37,9 +53,13 @@ boolean hasObject(int right, int left){
   return (right > 500 ||  left >500);
 }
 
-void turnOnLasers(){
+void turnLasersOn(){
   digitalWrite(rightLaser, HIGH);
   digitalWrite(leftLaser, HIGH);
+}
+void turnLasersOff(){
+  digitalWrite(rightLaser, LOW);
+  digitalWrite(leftLaser, LOW);
 }
 void startMotor(){
   digitalWrite(clockwiseEngine, HIGH);
